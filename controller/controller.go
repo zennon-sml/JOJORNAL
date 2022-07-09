1786453e43b24ac6f219ab74ca27f5655a1741fe
zennon-sml <zennonsampaioml@gmail.com>
2022-07-09 16:43:18 -0300

    CRUD terminado assim como as rotas,requests salvos no postman

--- controller/controller.go
+++ controller/controller.go
@@ -1,63 +1,19 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"strings"
-	"time"
-
 	"github.com/gin-gonic/gin"
 	"github.com/zennon-sml/JOJORNAL/models"
 )
 
-var artigos []models.Artigo
-
-func pegarArtigos(c *gin.Context) {
-	c.JSON(http.StatusOK, artigos)
-}
-
 func main() {
-	models.PegarTodosArtigos()
 	// models.FazerArtigo(fazerBixo())
+
 	router := gin.Default()
-	router.GET("/artigos", mostrarArtigos)
+	//routes
+	router.GET("/artigos", models.PegarTodosArtigos)
+	router.GET("/artigos/:id", models.PegarArtigo)
+	router.DELETE("/artigos/:id", models.ApagarArtigo)
+	router.PUT("/artigos", models.AtualizarArtigo)
+	router.POST("/artigos", models.FazerArtigo)
 	router.Run(":8080")
-}
-
-func fazerBixo() models.Artigo {
-	reader := bufio.NewReader(os.Stdin)
-	var art models.Artigo
-	var lines []string
-	for i := 0; i < 2; i++ {
-		if i == 0 {
-			fmt.Print("Titulo: ")
-		} else {
-			fmt.Print("Conteudo: ")
-		}
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			log.Fatal(err)
-		}
-		if len(strings.TrimSpace(line)) == 0 {
-			break
-		}
-		lines = append(lines, line)
-	}
-	art.Titulo = lines[0]
-	art.Conteudo = lines[1]
-	art.Criado = time.Now()
-	return art
-}
-
-func criarArtigo(c *gin.Context) {
-	var artigo models.Artigo
-	c.BindJSON(&artigo)
-}
-
-func mostrarArtigos(c *gin.Context) {
-	artigos := models.PegarTodosArtigos()
-	c.BindJSON(&artigos)
 }
--- models/artigo.go
+++ models/artigo.go
@@ -1,36 +1,68 @@
 package models
 
 import (
-	"fmt"
+	"log"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 type Artigo struct {
 	ID       uint64    `gorm:"primarykey:auto_increment" json:"id"`
-	Titulo   string    `gorm:"type:varchar(100)" json:"title"`
-	Conteudo string    `json:"content"`
-	Criado   time.Time `json:"criado"`
+	Titulo   string    `gorm:"type:varchar(100)" json:"titulo"`
+	Conteudo string    `json:"conteudo"`
+	Criado   time.Time `gorm:"autoCreateTime" json:"criado"`
 }
 
-func FazerArtigo(a Artigo) {
+func FazerArtigo(c *gin.Context) {
 	bd := fazerCon()
-	bd.AutoMigrate(&Artigo{})
-	novoArtigo := bd.Create(&a)
-	// if err := bd.Create(&a).Error; err != nil {
-	// 	log.Fatal("ERRO NO CREATE", err)
-	// }
-	fmt.Println("artigo feito: ", novoArtigo)
+
+	var artigo Artigo
+
+	if err := c.BindJSON(&artigo); err != nil {
+		log.Fatal("ERRO FAZENDO ARTIGO, ", err)
+	}
+	bd.Create(&artigo)
+	c.IndentedJSON(201, artigo)
 }
 
-func PegarTodosArtigos() []Artigo {
+func PegarArtigo(c *gin.Context) {
+	id := c.Param("id")
 	bd := fazerCon()
-	bd.AutoMigrate(&Artigo{})
+	var artigo Artigo
+	bd.First(&artigo, id)
+
+	c.IndentedJSON(200, artigo)
+}
+
+func PegarTodosArtigos(c *gin.Context) {
+	bd := fazerCon()
 	var artigos []Artigo
 	bd.Find(&artigos)
-	// for _, a := range artigos {
-	// 	dia := a.Criado.Day()
-	// 	fmt.Print(a.ID, "-", a.Titulo, "dia: ", dia)
-	// 	fmt.Printf("\n%T %T\n", a.ID, a.Titulo)
-	// }
-	return artigos
+	c.IndentedJSON(200, artigos)
+}
+
+//TODO em vez de pegar o id pela url mandar todo a struct
+func ApagarArtigo(c *gin.Context) {
+	id := c.Param("id")
+	bd := fazerCon()
+	var artigo Artigo
+	bd.Delete(&artigo, id)
+
+	c.IndentedJSON(204, artigo)
+}
+
+func AtualizarArtigo(c *gin.Context) {
+	bd := fazerCon()
+	var artigoNovo Artigo
+	if err := c.BindJSON(&artigoNovo); err != nil {
+		log.Fatal("ERRO FAZENDO ARTIGO, ", err)
+	}
+	var artigoVelho Artigo
+	bd.First(&artigoVelho, artigoNovo.ID)
+	artigoVelho.Titulo = artigoNovo.Titulo
+	artigoVelho.Conteudo = artigoNovo.Conteudo
+	bd.Save(&artigoVelho)
+
+	c.IndentedJSON(201, artigoVelho)
 }
